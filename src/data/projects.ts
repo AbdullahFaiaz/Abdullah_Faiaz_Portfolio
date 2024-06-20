1fae3faa7f2bbbd18d711065e06871ad00a364e2
AbdullahFaiaz <talha0184999@gmail.com>
2024-06-21 05:31:55 +0600

    github links updated

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,95 +1,4 @@
-// import { type ProjectCardProps } from "@/components/projects/project-card";
-// import { type ProjectShowcaseListItem } from "@/components/projects/project-showcase-list";
 
-// export const PROJECT_SHOWCASE: ProjectShowcaseListItem[] = [
-//   {
-//     index: 0,
-//     title: "Urban Estates",
-//     href: "/projects",
-//     tags: ["React JS", "Tailwind CSS", "MongoDb", "Express", "Node JS", "Firebase",],
-//     image: {
-//       LIGHT: "/images/projects/US.webp",
-//       DARK: "/images/projects/US.webp",
-//     },
-//   },
-//   {
-//     index: 1,
-//     title: "Volunteer Hub",
-//     href: "/projects",
-//     tags: ["React JS", "Tailwind CSS", "MongoDb", "Express", "Node JS", "Firebase"],
-//     image: {
-//       LIGHT: "/images/projects/VH1.webp",
-//       DARK: "/images/projects/VH1.webp",
-//     },
-//   },
-//   {
-//     index: 2,
-//     title: "Adventure Avenue",
-//     href: "/projects",
-//     tags: ["React JS", "Tailwind CSS", "MongoDb", "Express", "Node JS", "Firebase"],
-//     image: {
-//       LIGHT: "/images/projects/AA1.webp",
-//       DARK: "/images/projects/AA1.webp",
-//     },
-//   },
-// ];
-
-// export const PROJECTS_CARD: ProjectCardProps[] = [
-//   {
-//     name: "Urban Estates",
-//     favicon: "/images/projects/logos/logo.ico",
-//     imageUrl: [
-//       "/images/projects/US.webp",
-//       "/images/projects/US1.webp",
-//       "/images/projects/US2.webp",
-//       "/images/projects/US3.webp",
-//     ],
-//     description:
-//       "Urban Estates creates a platform where agents can showcase properties for sale, and users can explore, review, and purchase their dream homes. Admins oversee the verification of properties and manage user roles, ensuring a secure and efficient marketplace.",
-//     sourceCodeHref: "https://github.com/AbdullahFaiaz",
-//     liveWebsiteHref: "https://m-52-5-auth.web.app/",
-//   },
-//   {
-//     name: "Volunteer Hub",
-//     favicon: "/images/projects/logos/VH.ico",
-//     imageUrl: [
-//       "/images/projects/VH1.webp",
-//       "/images/projects/VH2.webp",
-//       "/images/projects/VH3.webp",
-//       "/images/projects/VH4.webp",
-//     ],
-//     description:
-//       "This website crates a platform where organizers can post their volunteer needs and individuals can respond to those needs.",
-//     sourceCodeHref: "https://github.com/AbdullahFaiaz/Volunteer-Hub-Client",
-//     liveWebsiteHref: "https://volunteer-hub-acc94.web.app/",
-//   },
-//   {
-//     name: "Adventure Avenue",
-//     favicon: "/images/projects/logos/AA.ico",
-//     imageUrl: [
-//       "/images/projects/AA1.webp",
-//       "/images/projects/AA2.webp",
-//       "/images/projects/AA2.webp",
-//     ],
-//     description:
-//       "This website showcases wonderful tourist spots located in Southeast Asia. With a user-friendly interface, it allows travel enthusiasts to explore, add, remove, or edit tourist spots based on their experiences and insights.",
-//     sourceCodeHref: "https://github.com/AbdullahFaiaz/Adventure-Avenue-Client-Side",
-//     liveWebsiteHref: "https://adventureavenue-76979.web.app/",
-//   },
-//   {
-//     name: "My portfolio",
-//     favicon: "/images/projects/logos/HH.ico",
-//     imageUrl: [
-//       "/images/projects/HH1.webp",
-//       "/images/projects/HH2.webp",
-//       "/images/projects/HH3.webp",
-//     ],
-//     description:
-//       "This is a real estate-related website for residential housings that allows users to browse real estate properties, view detailed property information, and manage their own profiles and save properties information.",
-//     sourceCodeHref: "https://github.com/AbdullahFaiaz/Harmony-Homes",
-//     liveWebsiteHref: "https://assignment-9-f42a0.web.app/",
-//   },
-// ];
 import { type ProjectCardProps } from "@/components/projects/project-card";
 import { type ProjectShowcaseListItem } from "@/components/projects/project-showcase-list";
 
@@ -159,7 +68,7 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
     ],
     description:
       "Urban Estates creates a platform where agents can showcase properties for sale, and users can explore, review, and purchase their dream homes. Admins oversee the verification of properties and manage user roles, ensuring a secure and efficient marketplace.",
-    sourceCodeHref: "https://github.com/AbdullahFaiaz",
+    sourceCodeHref: "https://github.com/AbdullahFaiaz/Urban-Estates",
     liveWebsiteHref: "https://m-52-5-auth.web.app/",
   },
   {
@@ -173,7 +82,7 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
     ],
     description:
       "This website creates a platform where organizers can post their volunteer needs and individuals can respond to those needs.",
-    sourceCodeHref: "https://github.com/AbdullahFaiaz/Volunteer-Hub-Client",
+    sourceCodeHref: "https://github.com/AbdullahFaiaz/Volunteer-Hub",
     liveWebsiteHref: "https://volunteer-hub-acc94.web.app/",
   },
   {
@@ -186,8 +95,7 @@ export const PROJECTS_CARD: ProjectCardProps[] = [
     ],
     description:
       "This website showcases wonderful tourist spots located in Southeast Asia. With a user-friendly interface, it allows travel enthusiasts to explore, add, remove, or edit tourist spots based on their experiences and insights.",
-    sourceCodeHref:
-      "https://github.com/AbdullahFaiaz/Adventure-Avenue-Client-Side",
+    sourceCodeHref: "https://github.com/AbdullahFaiaz/Adventure-Avenue",
     liveWebsiteHref: "https://adventureavenue-76979.web.app/",
   },
   {
