dcaf3b4c5a2f125745410d49128e8fcd1b52d076
abdullahfaiaz <talha0184999@gmail.com>
2024-09-14 19:47:59 +0600

    typescript added in the skills section

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,125 +1,3 @@
-// import {SiExpress,SiNextdotjs} from "react-icons/si";
-// import {FramerMotionIcon,ReactRouterDomIcon} from "@/components/icons";
-// import { type SkillsShowcaseProps } from "@/components/skills/skills-showcase";
-
-// // Languages
-// import HtmlSvg from "@/public/icons/html.svg";
-// import CsssSvg from "@/public/icons/css.svg";
-// import JavascriptSvg from "@/public/icons/javascript.svg";
-// import PythonSvg from "@/public/icons/python.svg";
-
-// // Libraries
-// import ReactjsSvg from "@/public/icons/reactjs.svg";
-// import TailwindcssSvg from "@/public/icons/tailwindcss.svg";
-// import MuiSvg from "@/public/icons/mui.svg";
-// import ViteSvg from "@/public/icons/vite.svg";
-
-// // Backend
-// import NodejsSvg from "@/public/icons/nodejs.svg";
-
-// // Database and ORMS
-// import MongoDBSvg from "@/public/icons/mongodb.svg";
-
-// // Tools and Tech
-// import GitSvg from "@/public/icons/git.svg";
-// import PostmanSvg from "@/public/icons/postman.svg";
-// import Firebase from "@/public/icons/firebase.svg";
-
-// export const SKILLS_DATA: SkillsShowcaseProps["skills"] = [
-//   {
-//     sectionName: "Languages",
-//     skills: [
-//       {
-//         name: "HTML",
-//         icon: HtmlSvg,
-//       },
-//       {
-//         name: "CSS",
-//         icon: CsssSvg,
-//       },
-//       {
-//         name: "Javascript",
-//         icon: JavascriptSvg,
-//       },
-//       {
-//         name: "Python",
-//         icon: PythonSvg,
-//       },
-//     ],
-//   },
-//   {
-//     sectionName: "Libraries and Frameworks",
-//     skills: [
-//       {
-//         name: "Reactjs",
-//         icon: ReactjsSvg,
-//       },
-//       {
-//         name: "Nextjs",
-//         icon: SiNextdotjs,
-//       },
-//       {
-//         name: "React Router Dom",
-//         icon: ReactRouterDomIcon,
-//       },
-//       {
-//         name: "Tailwindcss",
-//         icon: TailwindcssSvg,
-//       },
-//       {
-//         name: "MUI",
-//         icon: MuiSvg,
-//       },
-//       {
-//         name: "Framer motion",
-//         icon: FramerMotionIcon,
-//       },
-//       {
-//         name: "Vite",
-//         icon: ViteSvg,
-//       },
-//     ],
-//   },
-//   {
-//     sectionName: "Backend",
-//     skills: [
-//       {
-//         name: "Nodejs",
-//         icon: NodejsSvg,
-//       },
-//       {
-//         name: "Express",
-//         icon: SiExpress,
-//       },
-//     ],
-//   },
-//   {
-//     sectionName: "Databases",
-//     skills: [
-//       {
-//         name: "MongoDB",
-//         icon: MongoDBSvg,
-//       },
-//       {
-//         name: "Firebase",
-//         icon: Firebase,
-//       },
-//     ],
-//   },
-//   {
-//     sectionName: "Tools and Technologies",
-//     skills: [
-//       {
-//         name: "Git",
-//         icon: GitSvg,
-//       },
-//       {
-//         name: "Postman",
-//         icon: PostmanSvg,
-//       },
-//     ],
-//   },
-// ];
 import { SiExpress, SiNextdotjs } from "react-icons/si";
 import { FramerMotionIcon, ReactRouterDomIcon } from "@/components/icons";
 import { type SkillsShowcaseProps } from "@/components/skills/skills-showcase";
@@ -128,6 +6,7 @@ import { type SkillsShowcaseProps } from "@/components/skills/skills-showcase";
 import HtmlSvg from "@/public/icons/html.svg";
 import CssSvg from "@/public/icons/css.svg"; // Corrected the typo from CsssSvg to CssSvg
 import JavascriptSvg from "@/public/icons/javascript.svg";
+import TypescriptSvg from "@/public/icons/typescript.svg";
 import PythonSvg from "@/public/icons/python.svg";
 
 // Libraries
@@ -162,6 +41,10 @@ export const SKILLS_DATA: SkillsShowcaseProps["skills"] = [
       {
         name: "Javascript",
         icon: JavascriptSvg,
+      },
+      {
+        name: "Typescript",
+        icon: TypescriptSvg,
       },
       {
         name: "Python",
